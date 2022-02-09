188604dec03d4c4d903781f24ef086ffc1139b55
unknown <lyuziwen2@gmail.com>
2022-02-09 00:18:38 -0500

    add query to get the retired staff info

--- schema.sql
+++ schema.sql
@@ -49,3 +49,22 @@ CREATE TABLE titles(
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 SELECT * FROM departments;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create retirement new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
